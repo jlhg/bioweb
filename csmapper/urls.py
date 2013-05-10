7753c3c3829e3160f504971f0835e6616727bcfd
Jian-Long Huang <jianlong@ntu.edu.tw>
2013-05-11 02:16:15 +0800

    Reverting "For camap v1.8" Revert "For camap v1.8"
    This reverts commit 7d83ccc499c9129c3f876cc327d762e96e704481.

--- csmapper/urls.py
+++ csmapper/urls.py
@@ -3,5 +3,5 @@ from csmapper import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
-                       url(r'^upload/', views.upload, name='upload'),
+                       url(r'^upload/', views.upload, name='upload')
                        )
